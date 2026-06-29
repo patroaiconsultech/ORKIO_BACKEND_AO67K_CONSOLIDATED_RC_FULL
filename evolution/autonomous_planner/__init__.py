f150ba3a39be399d1d9aa2ea4d52197a939902e2
patroaiconsultech <patroaiconsultech@gmail.com>
2026-06-29 17:43:51 +0000

    OEP-007.1 Planner Safety Gate PASS

--- evolution/autonomous_planner/__init__.py
+++ evolution/autonomous_planner/__init__.py
@@ -1,8 +1,12 @@
-from evolution.autonomous_planner.models import Plan, PlanStep
+from evolution.autonomous_planner.models import AutonomousPlan, PlanStep
 from evolution.autonomous_planner.service import AutonomousPlannerService, autonomous_planner_service
 from evolution.autonomous_planner.safety_gate import PlannerSafetyError, PlannerSafetyGate, planner_safety_gate
 
+# Backward-compatible alias
+Plan = AutonomousPlan
+
 __all__ = [
+    "AutonomousPlan",
     "Plan",
     "PlanStep",
     "AutonomousPlannerService",
